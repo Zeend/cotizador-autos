c80875c8a816a4fba011bf9754a509727759f878
Jorge <jorge@Mac-mini-de-Jorge.local>
2019-05-27 18:34:58 -0500

    Updates

---
+++ precache-manifest.2a288b71dbd0ab500aa3a166ef010295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b100f6f710e2816a31679bb272573846",
+    "url": "/cotizador-autos/index.html"
+  },
+  {
+    "revision": "f95402982244416c841d",
+    "url": "/cotizador-autos/static/css/main.f32fa962.chunk.css"
+  },
+  {
+    "revision": "a70e87d0681534edc7bd",
+    "url": "/cotizador-autos/static/js/2.23cb5ec5.chunk.js"
+  },
+  {
+    "revision": "f95402982244416c841d",
+    "url": "/cotizador-autos/static/js/main.f210ac4d.chunk.js"
+  },
+  {
+    "revision": "740c189f558540436e41",
+    "url": "/cotizador-autos/static/js/runtime~main.0702e234.js"
+  }
+]);
